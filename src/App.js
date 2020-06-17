0d718c97348080f178f060c3f559afc736019fc1
Giorgio Acosta <gioacostax@gmail.com>
2020-06-16 20:19:50 -0500

    Material

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      // list
-    </div>
-  );
+  return <Button color="primary">Hello World</Button>;
 }
 
 export default App;
