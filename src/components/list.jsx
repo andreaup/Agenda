1be347d7afa892b51a2e62d406d99c261714f77a
Giorgio Acosta <gioacostax@gmail.com>
2020-06-16 20:36:52 -0500

    Add list component

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,3 +1,21 @@
-// cards
+import React from 'react';
+import Card from './card';
 
-props.list.map(item => <Card info={item.info} />);
+export default function App({ items }) {
+  return (
+    <div className="list-cards">
+      {
+        items.map((item) => (
+          <Card
+            name={item.nombre}
+            lastName={item.lastName}
+            city={item.city}
+            profession={item.profession}
+            entryDate={item.entryDate}
+            photo={item.photo}
+          />
+        ))
+      }
+    </div>
+  )
+}
